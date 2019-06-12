df39d00622a9e162455c9e11d3c2ed66e00dc9b5
zhu <819508408@qq.com>
2019-06-12 10:20:46 +0800

    新增测点

--- pages/test/testList/addTestPoint.js
+++ pages/test/testList/addTestPoint.js
@@ -5,7 +5,9 @@ new Page({
    * 页面的初始数据
    */
   data: {
-    pileNo:"" 
+    pileNo:"",
+    showTopTips:false,
+    erroInfo:"错误提示"
   },
 
   /**
@@ -48,8 +50,48 @@ new Page({
    this.setData({
      'pileNo': data.pileNo
    });
+    var erroInfo;
+    if (!data.pileNo){
+      erroInfo="请填写轴线位置";
+      this.errorTips(erroInfo);
+      return;
+    }
+    if (!data.testLoad) {
+      erroInfo = "请填写试验最大荷载";
+      this.errorTips(erroInfo);
+      return;
+    }
+    if (!data.height1) {
+      erroInfo = "请填写设计基底标高";
+      this.errorTips(erroInfo);
+      return;
+    }
+    if (!data.height2) {
+      erroInfo = "请填写检测起始试验标高";
+      this.errorTips(erroInfo);
+      return;
+    }
+    if (!data.pileBearing) {
+      erroInfo = "请填写地基承载力特征值";
+      this.errorTips(erroInfo);
+      return;
+    }
     data.entrustId = entrustId;
+
     this.submit(data);
+  },
+  //错误提示
+  errorTips: function (erroInfo){
+    var that = this;
+    this.setData({
+      showTopTips: true,
+      erroInfo: erroInfo
+    });
+    setTimeout(function () {
+      that.setData({
+        showTopTips: false
+      });
+    }, 3000);
   },
   //提交数据
   submit:function(data){
@@ -105,5 +147,10 @@ new Page({
           console.log('接口调用失败');
         }
       })
+  },
+  cancel:function(){
+    wx.navigateBack({
+      delta: 1 //想要返回的层级
+    })
   }
 })
